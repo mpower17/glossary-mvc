a5facba18119911b0022d78d5d951aa980857d4d
mzaikov <mzaikov@payneteasy.com>
2022-01-31 00:20:37 +0300

    fix

--- src/main/java/com/jpt/glossary/repository/TermRepository.java
+++ src/main/java/com/jpt/glossary/repository/TermRepository.java
@@ -3,11 +3,14 @@ package com.jpt.glossary.repository;
 import com.jpt.glossary.model.Term;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.annotation.Nonnull;
+
 public interface TermRepository extends CrudRepository<Term, String> {
-    
-    @Override
-    void delete(Term term);
 
     @Override
-    <S extends Term> Iterable<S> saveAll(Iterable<S> entities);
+    void delete(@Nonnull Term term);
+
+    @Override
+    @Nonnull
+    <S extends Term> Iterable<S> saveAll(@Nonnull Iterable<S> entities);
 }
